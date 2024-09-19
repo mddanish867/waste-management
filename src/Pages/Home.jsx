a9932a36d98df9fa0b58970f5f651b8aaa0e523d
mddanish867 <mddanish867@gmail.com>
2024-09-19 18:19:07 +0530

    modified hope page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,10 @@ import MainContent from "./MainContent";
 import Footer from "./Footer";
 export default function Home() {
   return (
-    <div className="p-4">
+    <>
       <Header />
       <MainContent />
       <Footer />
-    </div>
+    </>
   );
 }
